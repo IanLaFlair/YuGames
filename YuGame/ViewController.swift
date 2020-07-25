21eba87d7b309aaa710eefcf14589d7cd303d41c
Ian Rachman Dana <rahmandana08@gmail.com>
2020-07-25 11:30:47 +0700

    Initial Commit

---
+++ YuGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YuGame
+//
+//  Created by MacBook Pro on 25/07/20.
+//  Copyright © 2020 KaryaLokalStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
